cbad972621698733d8dbda9d0439cf143fec0518
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-07 18:36:03 -0400

    modified dojo.html (added form data) modified landing_page.py to link to dojo.html

--- landing_page.py
+++ landing_page.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 app =  Flask(__name__)
 
-@app.route('/')
-					
+@app.route('/')			
 def landing():
 	return render_template('index.html')
 
-@app.route('/')
+@app.route('/ninjas')
 def success():
-	return render_template('index.html')
+	return render_template('ninjas.html')
+
+@app.route('/dojos/new')
+def success():
+	return render_template('dojos.html')
 
 app.run(debug=True)
